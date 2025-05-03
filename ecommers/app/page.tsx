f0c87e2f062a57dc2c2e30a80bd3286b568e206a
Im-sameer9024 <rskkhan89@gmail.com>
2025-05-03 11:47:23 +0530

    newProject

--- ecommers/app/page.tsx
+++ ecommers/app/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Input } from '../components/ui/input'
+import { Button } from '../components/ui/button'
+import { Textarea } from '../components/ui/textarea'
 
 const Home = () => {
   return (
     <div>
-      
+      Hello
+      <Input/>
+      <Button variant="elevated">
+        Hello
+      </Button>
+      <Textarea>
+        
+      </Textarea>
     </div>
   )
 }
