be895528296f4ba41e55ac95002c84db3d1509b5
Mohammed Sameer <rskkhan89@gmail.com>
2025-06-27 01:23:11 +0530

    updates

---
+++ groceryapp/src/components/hooks/useNavbar.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { usePathname, useRouter } from "next/navigation";
+import  { useCallback, useState } from "react";
+
+const useNavbar = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
+  const path = usePathname();
+  const router = useRouter();
+
+  "hello"
+
+  const handleNavigation = useCallback(
+    (url: string) => {
+      setShowSidebar(false);
+      router.push(url);
+    },
+    [router]
+  );
+
+  const handleClose = useCallback(() => {
+    setShowSidebar(false);
+  }, []);
+
+  return {
+    showSidebar,
+    path,
+    handleClose,
+    handleNavigation,
+    setShowSidebar
+  };
+};
+
+export default useNavbar;
